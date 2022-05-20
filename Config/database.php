212436368f7e5cc1b4adcab5a0cb48de30e6db44
N3f73r <n3f73r@gmail.com>
2022-05-20 11:12:17 -0500

    database_bug

--- Config/database.php
+++ Config/database.php
@@ -1,7 +1,8 @@
 <?php 
 	class Conectar {
 		public static function conection (){
-			$conexion = new PDO("mysql:host=ec2-54-204-56-171.compute-1.amazonaws.com;port=5432;dbname=dao79h7ujhca5l", "hajeywzaybxale", "ea352c20f81abd4606004dd94bc649626389b6d325d2732340ddfe59792deaf4");
+			$conexion = new PDO("pgsql:host=ec2-54-204-56-171.compute-1.amazonaws.com;port=5432;dbname=dao79h7ujhca5l", "hajeywzaybxale", "ea352c20f81abd4606004dd94bc649626389b6d325d2732340ddfe59792deaf4");
+			$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			return $conexion;
 		}
 	}
