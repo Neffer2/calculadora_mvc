9732948eec9c4e6895f38082075c14430c1a919a
N3f73r <n3f73r@gmail.com>
2022-05-20 11:56:30 -0500

    fnial

--- Config/database.php
+++ Config/database.php
@@ -1,14 +1,14 @@
 <?php 
 	class Conectar {
-		// public static function conection (){
-		// 	$conexion = new PDO("mysql:host=localhost;dbname=prueba_php", "root", "");
-		// 	return $conexion;
-		// }
-
 		public static function conection (){
-			$conexion = new PDO("pgsql:host=ec2-54-204-56-171.compute-1.amazonaws.com;port=5432;dbname=dao79h7ujhca5l", "hajeywzaybxale", "ea352c20f81abd4606004dd94bc649626389b6d325d2732340ddfe59792deaf4");
-			$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$conexion = new PDO("mysql:host=localhost;dbname=prueba_php", "root", "");
 			return $conexion;
 		}
+
+		// public static function conection (){
+		// 	$conexion = new PDO("pgsql:host=ec2-54-204-56-171.compute-1.amazonaws.com;port=5432;dbname=dao79h7ujhca5l", "hajeywzaybxale", "ea352c20f81abd4606004dd94bc649626389b6d325d2732340ddfe59792deaf4");
+		// 	$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		// 	return $conexion;
+		// }
 	}
 ?>
