675273e86574a0bc485469c8885de1cb99621fef
N3f73r <n3f73r@gmail.com>
2022-05-20 11:04:31 -0500

    database_bug

--- Config/database.php
+++ Config/database.php
@@ -1,7 +1,7 @@
 <?php 
 	class Conectar {
 		public static function conection (){
-			$conexion = new PDO("mysql:host=ec2-54-204-56-171.compute-1.amazonaws.com;dbname=dao79h7ujhca5l", "hajeywzaybxale", "ea352c20f81abd4606004dd94bc649626389b6d325d2732340ddfe59792deaf4");
+			$conexion = new PDO("mysql:host=ec2-54-204-56-171.compute-1.amazonaws.com;port=5432;dbname=dao79h7ujhca5l", "hajeywzaybxale", "ea352c20f81abd4606004dd94bc649626389b6d325d2732340ddfe59792deaf4");
 			return $conexion;
 		}
 	}
